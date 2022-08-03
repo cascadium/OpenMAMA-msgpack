77d7d99dbc3d6585fb5a51f468669047cd572087
Frank Quinn <fquinn@cascadium.io>
2022-08-03 08:46:24 +0100

    Tentative fixes for osx

--- devops/build/ci-run.macos.sh
+++ devops/build/ci-run.macos.sh
@@ -7,13 +7,16 @@ set -x
 
 set -e
 
-brew install openmama
+brew install apr apr-util msgpack
 
+APR_ROOT=$(find /usr/local/Cellar/apr -type d -name libexec)
+APRUTIL_ROOT=$(find /usr/local/Cellar/apr-util -type d -maxdepth 1 -mindepth 1)
+MSGPACK_ROOT=$(find /usr/local/Cellar/msgpack -type d -maxdepth 1 -mindepth 1)
 
 mkdir bld
 cd bld
 export DYLD_LIBRARY_PATH=$(pwd)/install/lib:$(echo /usr/local/Cellar/openmama/*)/lib
-cmake -DCMAKE_INSTALL_PREFIX=$(pwd)/install -DMAMA_ROOT=$(echo /usr/local/Cellar/openmama/*) ..
+cmake -DCMAKE_INSTALL_PREFIX=$(pwd)/install -DAPR_ROOT=$APR_ROOT -DAPRUTIL_ROOT=$APRUTIL_ROOT -DMSGPACK_ROOT=$MSGPACK_ROOT ..
 make -j
 make install
 ctest . -E MsgFieldVectorBoolTests.GetVectorBoolNullField --timeout 120 --output-on-failure
