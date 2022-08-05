61f869431d38631d3313e75805659c039cda77c9
Frank Quinn <fquinn@cascadium.io>
2022-08-05 09:13:48 +0100

    Added wombatcommon path to ci run script

--- devops/build/ci-run.bat
+++ devops/build/ci-run.bat
@@ -8,7 +8,7 @@ vcpkg install libevent:%PLATFORM%-windows apr:%PLATFORM%-windows apr-util:%PLATF
 mkdir %BUILD_DIR% || goto error
 cd %BUILD_DIR% || goto error
 
-set PATH=%BUILD_DIR%\src\RelWithDebInfo;%BUILD_DIR%\_deps\openmamaomnm-build\src\RelWithDebInfo;%BUILD_DIR%\_deps\openmama-build\mama\c_cpp\src\c\RelWithDebInfo;C:\Program Files (x86)\Gow\bin;C:\vcpkg\installed\%PLATFORM%-windows\bin;%OPENMAMA_INSTALL_DIR%\bin;%PATH%
+set PATH=%BUILD_DIR%\src\RelWithDebInfo;%BUILD_DIR%\_deps\openmamaomnm-build\src\RelWithDebInfo;%BUILD_DIR%\_deps\openmama-build\common\c_cpp\src\c\RelWithDebInfo;%BUILD_DIR%\_deps\openmama-build\mama\c_cpp\src\c\RelWithDebInfo;C:\Program Files (x86)\Gow\bin;C:\vcpkg\installed\%PLATFORM%-windows\bin;%OPENMAMA_INSTALL_DIR%\bin;%PATH%
 
 cmake -G "%GENERATOR%" %EXTRA_ARGS% -DCMAKE_TOOLCHAIN_FILE=c:/vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=%OPENMAMA_INSTALL_DIR% -DGTEST_ROOT=C:/vcpkg/installed/%PLATFORM%-windows/ -DMAMA_ROOT=C:/vcpkg/installed/%PLATFORM%-windows/ -DAPR_ROOT=C:/vcpkg/installed/%PLATFORM%-windows/ -DAPRUTIL_ROOT=C:/vcpkg/installed/%PLATFORM%-windows/ .. || goto error
 cmake --build . --config RelWithDebInfo --target install || goto error
